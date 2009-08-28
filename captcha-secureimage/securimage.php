6b321aa32980350870658af00d587d0ba61cf7c4
MikeChallis <MikeChallis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-08-28 20:16:05 +0000

    typo
    git-svn-id: https://plugins.svn.wordpress.org/si-captcha-for-wordpress/trunk@150128 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- captcha-secureimage/securimage.php
+++ captcha-secureimage/securimage.php
@@ -38,8 +38,8 @@
   ChangeLog
 
   1.0.3.1m.2 - (Aug 28, 2009) Mike Challis fix http://www.642weather.com/weather/scripts.php
-  - Added Auto fail over to $use_gd_font if the PHP installation is configured without "--with-ttf".
-  Some users were reporting there was no error indicating this condition and the capcha was not working.
+  - Added Automatic fail over to GD Fonts instead of TTF Fonts if the PHP installation is configured without "--with-ttf".
+  Some users were reporting there was no error indicating this condition and the captcha was not working.
 
   1.0.3.1m.1 - (Apr 06, 2009) Mike Challis fix http://www.642weather.com/weather/scripts.php
   - Fixed audio CAPTCHA link URL in securimage_play.php, it did not work properly on Safari 3.2.1 (Mac OS X 10.5.6).
