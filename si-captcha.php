938fafa3a9448c6ed62ff3f47a04927a95e34371
MikeChallis <MikeChallis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-09-26 06:49:44 +0000

    bp login adjust style
    git-svn-id: https://plugins.svn.wordpress.org/si-captcha-for-wordpress/trunk@158084 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- si-captcha.php
+++ si-captcha.php
@@ -542,7 +542,7 @@ if ($this->si_captcha_check_requires()) {
 
 // the captcha html - buddypress login form
 echo '
-<div style="width: 350px;  height: 45px">';
+<div style="width: 425px;  height: 45px">';
 $this->si_captcha_captcha_html('si_image_login');
 echo '<input type="text" name="captcha_code" id="captcha_code" class="input" '.$si_aria_required.' style="width:65px;" />
          <label for="captcha_code">';
