6c9cce74d96af61da23a81e9f36153e85a2f5377
MikeChallis <MikeChallis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-23 21:41:20 +0000

    git-svn-id: https://plugins.svn.wordpress.org/si-contact-form/trunk@480015 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- si-contact-form.php
+++ si-contact-form.php
@@ -97,7 +97,7 @@ function si_contact_options_page() {
  * In the end, It stores the form data.
  */
 function vcita_validate_initialized_user($form_num, $form_params, $auto_install) {
- 
+
   // Check if a initializtion is required
   if (!isset($form_params['vcita_initialized']) || $form_params['vcita_initialized'] == 'false') {
 
@@ -108,7 +108,7 @@ function vcita_validate_initialized_user($form_num, $form_params, $auto_install)
 		
     } else {
 	  if ($this->vcita_check_expert_available($form_params)) {
-		$form_params['vcita_enabled'] =  'true';
+	   	$form_params['vcita_enabled'] =  'true';
 	  }
 	  
 	  $form_params['vcita_initialized'] = 'true'; // Stop initialize if upgrade process
@@ -1360,7 +1360,7 @@ function si_contact_get_options($form_num) {
          'max_forms' => '4',
          'max_fields' => '4',
          'captcha_disable_session' => 'true',
-		 'vcita_auto_install' => 'true', /* --- vCita Global Settings --- */
+		 'vcita_auto_install' => 'false', /* --- vCita Global Settings --- */
 		 'vcita_version' => '1.0',
       );
 
